ebb3f0852aff540a308cc2b833c557415d14344a
Denis Skidan <skidan147@gmail.com>
2023-09-19 17:51:55 +0300

    test added

---
+++ src/test/java/ru/netology/selenide/AppCardDeliveryTest.java
@@ -0,0 +1,45 @@
+package ru.netology.selenide;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class AppCardDeliveryTest {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+
+    @Test
+    void shouldMakeAnAppointment() throws InterruptedException {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Москва");
+        String planningDate = generateDate(5, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue("Пуздой Акакий");
+        $("[data-test-id=phone] input").setValue("+79265642380");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
+
+
+    }
+
+
+}
